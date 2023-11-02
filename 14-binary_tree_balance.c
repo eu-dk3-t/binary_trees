65f8e6f503f9225a440392d03d7668730512743d
eu-dk3-t <ze_3fanuel@proton.me>
2023-11-02 17:56:26 +0000

    Removed Whitespace

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,15 +8,14 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        size_t l_size = 0;
-        size_t r_size = 0;
+	size_t l_size = 0;
+	size_t r_size = 0;
 
-        if (!tree)
-                return (0);
-
-        l_size += (tree->left) ? 1 + binary_tree_height(tree->left) : 0;
-        r_size += (tree->right) ? 1 + binary_tree_height(tree->right) : 0;
-        return ((l_size > r_size) ? l_size : r_size);
+	if (!tree)
+		return (0);
+	l_size += (tree->left) ? 1 + binary_tree_height(tree->left) : 0;
+	r_size += (tree->right) ? 1 + binary_tree_height(tree->right) : 0;
+	return ((l_size > r_size) ? l_size : r_size);
 }
 
 /**
